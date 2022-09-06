b10bb00becb5eaff7914fb96ffca94d49e3e0dac
LODS <lodsofcc@gmail.com>
2022-09-05 23:22:12 -0300

    feat: create dbmanager logic

--- class/DBManager.lua
+++ class/DBManager.lua
@@ -1,9 +1,18 @@
-Class = {}
+DBManager = {}
 
-function Class:new()
+function DBManager:new(dbName, directory)
     local instance = {}
 
-    
+    instance.dbName = dbName
+    instance.dbConnection = dbConnect('sqlite', directory)
+
+    setmetatable(instance, {
+        __index = DBManager
+    })
         
     return instance
+end
+
+function DBManager:getDB()
+    return self.dbConnection
 end
