2adba6390e30c3139247f592d97f2b035730bd9e
LODS <lodsofcc@gmail.com>
2022-09-08 01:10:24 -0300

    test: sql logic

--- sqlTest.lua
+++ sqlTest.lua
@@ -1,3 +1,9 @@
-local DBManager = Class:new()
-
-iprint(DBManager)
+addEventHandler('onResourceStart', resourceRoot, function()
+    local db = DBManager:new('dbTest', 'assets/database.db')
+    local repo = SQLRepo:new(db, 'tableTest')
+    
+    repo:create({
+        id = 1,
+        name = 'test'
+    })
+end)
