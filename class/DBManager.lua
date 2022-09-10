5a3605f5a2f06b7e0c20aa81672c2bf9f00e4e68
LODS <lodsofcc@gmail.com>
2022-09-10 14:29:01 -0300

    added new approach to MySQL

--- class/DBManager.lua
+++ class/DBManager.lua
@@ -1,10 +1,36 @@
-DBManager = {}
+local DBManager = {}
 
-function DBManager:new(dbName, directory)
+function DBManager:new(...)
     local instance = {}
+    local dto = {...}
 
-    instance.dbName = dbName
-    instance.dbConnection = dbConnect('sqlite', directory)
+    if (type(...) ~= 'table') then
+        instance.dbConnection = dbConnect(dto[1], dto[2] or 'database/file.db')
+    else
+        if (not dto[1]) then
+            return error('No database connection provided')
+        end
+        if (not dto[1].host or not dto[1].username or not dto[1].password or not dto[1].database) then
+            return error('Invalid database connection provided')
+        end
+        local host, port, username, password, database = dto[1].host, dto[1].port, dto[1].username, dto[1].password, dto[1].database
+        
+        instance.dbConnection = dbConnect(
+            'mysql',
+            'dbname=' .. database ..
+            ';host=' .. host ..
+            ';port=' .. port ..
+            ';charset=utf8',
+            username,
+            password
+        )
+
+        if (not instance.dbConnection) then
+            return error('Error while connecting to database')
+        end
+
+        outputDebugString('Connected to database')
+    end
 
     setmetatable(instance, {
         __index = self
@@ -15,4 +41,8 @@ end
 
 function DBManager:getDB()
     return self.dbConnection
+end
+
+function DBManagerClass()
+    return DBManager
 end
