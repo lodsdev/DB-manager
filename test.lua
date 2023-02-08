516a7d68b7fc0eb028104cfc1702b6f5e8719022
LODS <lodsofcc@gmail.com>
2023-02-08 17:25:32 -0300

    test: connect and gerate a table on db

--- test.lua
+++ test.lua
@@ -1,8 +1,32 @@
-addEventHandler('onResourceStart', resourceRoot, function()
-    local db = DBManagerClass("sqlite", 'database/db.sqlite')
-    local myTbl = TableClass(db:getConnection(), 'tests')
-    local service = RepoServiceClass(myTbl)
+local conn
+local Users
 
-    -- service:create({5, "LODIS", 20})
-    service:delete('id', 5)
+addEventHandler('onResourceStart', resourceRoot, function()
+    conn = DBManager:new({
+        dialect = 'sqlite',
+        storage = 'database/db.sqlite',
+    })
+    
+    if (not conn:getConnection()) then
+        error('DBManager: Connection failed', 2)
+    end
+
+    outputDebugString('DBManager: Connection successful')
+end)
+
+addCommandHandler('createTable', function(player, cmd)
+    Users = conn:define('users', {
+        id = {
+            type = DBManager.INTEGER,
+            primaryKey = true,
+            autoIncrement = true,
+            allowNull = true,
+        },
+        name = {
+            type = DBManager.STRING(32),
+            allowNull = false,
+            defaultValue = "LODS"
+        },
+        age = DBManager.INTEGER
+    })
 end)
