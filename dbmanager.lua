ef67b291c6e78970e84b7410a73bf655099a36bc
LODS <lodsofcc@gmail.com>
2023-03-04 14:43:07 -0300

    refactor(step1): cleaner code

--- dbmanager.lua
+++ dbmanager.lua
@@ -5,6 +5,7 @@
     
     MIT License
     Copyright (c) 2012-2022 Scott Chacon and others
+
     Permission is hereby granted, free of charge, to any person obtaining
     a copy of this software and associated documentation files (the
     "Software"), to deal in the Software without restriction, including
@@ -37,15 +38,10 @@ local function generateUUID()
         return s
     end
 
-    local function pad(str, len, char)
-        if (char == nil) then char = ' ' end
-        return string.rep(char, len - #str) .. str
-    end
-
     local function create()
         math.randomseed(os.time())
-        math.random() 
-        math.random() 
+        math.random()
+        math.random()
         math.random()
         local id0 = hex(math.random(0, 0xffff) + 0x10000)
         local id1 = hex(math.random(0, 0xffff) + 0x10000)
@@ -83,20 +79,16 @@ local function isNil(nilValue)
     return type(nilValue) == 'nil'
 end
 
-local function getTblSize(tbl)
-    local length = 0
-    for _ in pairs(tbl) do
-        length = length + 1
-    end
-    return length
+local function isNumber(number)
+    return type(number) == 'number'
 end
 
 local function async(f, callback, ...)
     local asyncCoroutine = coroutine.create(f)
     local function step(...)
         if (coroutine.status(asyncCoroutine) == 'dead') then
-            if (callback) then 
-                callback(...) 
+            if (callback) then
+                callback(...)
             end
         else
             local success, result = coroutine.resume(asyncCoroutine, ...)
@@ -141,9 +133,25 @@ end
 
 local function generateDateTime()
     local dateTimeTable = os.date('*t')
-    local dateTime = dateTimeTable.year .. addZeroForLessThan10(dateTimeTable.month) .. 
+    local dateTime = dateTimeTable.year .. addZeroForLessThan10(dateTimeTable.month) ..
     addZeroForLessThan10(dateTimeTable.day) .. addZeroForLessThan10(dateTimeTable.hour) .. addZeroForLessThan10(dateTimeTable.min) .. addZeroForLessThan10(dateTimeTable.sec)
     return dateTime
+end
+
+local function toSQLValue(value)
+    if (isString(value)) then
+        return "" .. value:gsub("'", "''") .. ""
+    elseif (isNumber(value)) then
+        return tostring(value)
+    elseif (isBoolean(value)) then
+        return value and '1' or '0'
+    elseif (isTable(value)) then
+        return "'" .. toJSON(value) .. "'"
+    elseif (isNil(value)) then
+        return 'NULL'
+    else
+        error('Invalid value type: ' .. type(value))
+    end
 end
 
 local CONSTRAINTS_DATA = {
@@ -177,25 +185,19 @@ local CONSTRAINT_ORDER = {
 }
 
 local function addConstraintToQuery(query, key, value)
-    local constraintOrderIndex = 1
-    local addedConstraint = false
-    while (constraintOrderIndex <= #CONSTRAINT_ORDER) do
-        local constraint = CONSTRAINT_ORDER[constraintOrderIndex]
-        if (constraint ~= 'type') then
-            if (constraint == 'allowNull' and value[constraint] == false) then
-                query = query .. " " .. CONSTRAINTS_DATA[constraint]
-            elseif (constraint ~= 'primaryKey' and constraint ~= 'allowNull' and value[constraint]) then
-                if (isBoolean(value[constraint])) then
-                    query = query .. " " .. CONSTRAINTS_DATA[constraint]
-                else
-                    query = query .. " " .. CONSTRAINTS_DATA[constraint] .. " " .. value[constraint]
-                end
+    local constraintsToAdd = {}
+    for i = 1, #CONSTRAINT_ORDER do
+        local constraint = CONSTRAINT_ORDER[i]
+        if (constraint ~= 'type' and value[constraint] ~= nil) then
+            if (constraint == 'allowNull' and not value[constraint]) then
+                constraintsToAdd[#constraintsToAdd+1] = CONSTRAINTS_DATA[constraint]
+            elseif (constraint ~= 'primaryKey' and constraint ~= 'allowNull') then
+                local constraintValue = isBoolean(value[constraint]) and '' or ' ' .. value[constraint]
+                constraintsToAdd[#constraintsToAdd+1] = CONSTRAINTS_DATA[constraint] .. constraintValue
             end
-            addedConstraint = true
         end
-        constraintOrderIndex = constraintOrderIndex + 1
     end
-    return query
+    return query .. ' ' .. table.concat(constraintsToAdd, ' ')
 end
 
 local DEBUG_RUNNING_DEFAULT = 'DBManager RUNNING (Default): '
@@ -210,24 +212,16 @@ local crud = {
             return exec
         end, function()
             local valuesInDB = self.valuesInDB
-            local strValues = ''
+            local strValues = table.concat(valuesInDB, ', ')
 
-            for key, value in ipairs(valuesInDB) do
-                if (key > 1) then
-                    strValues = strValues .. ', '
-                end
-                strValues = strValues .. '`' .. value .. '`'
-            end
-            
             local querySelect = 'SELECT ' .. strValues .. ' FROM `' .. self.tableName .. '` AS `' .. self.tableName .. '`'
             local prepareQuery = dbPrepareString(self.db:getConnection(), querySelect)
             if (not prepareQuery) then
                 return false
             end
-            
+
             local result, numAffectedRows, lastInsertId  = dbPoll(dbQuery(self.db:getConnection(), prepareQuery), -1)
-    
-            self.datas = result
+            self.datas = (#result > 0) and result or {}
             outputDebugString(DEBUG_RUNNING_DEFAULT .. querySelect)
 
             return result, numAffectedRows, lastInsertId
@@ -235,45 +229,27 @@ local crud = {
     end,
 
     create = function(self, data)
-        local query = 'INSERT INTO `' .. self.tableName .. '` ('
-        local values = ' VALUES ('
-        local i = 0
+        local keys = {}
+        local values = {}
 
         if (self.primaryKey) then
-            query = query .. "`" .. self.primaryKey .. "`, "
-            values = values .. "" .. #self.datas + 1 .. ", "
+            keys[#keys+1] = self.primaryKey
+            values[#values+1] = #self.datas + 1
         end
 
         for key, value in pairs(data) do
-            if (i > 0) then
-                query = query .. ', '
-                values = values .. ', '
-            end
-
-            query = query .. "`" .. key .. "`"
-
-            if (isString(value)) then
-                value = "'" .. value .. "'"
-            elseif (isBoolean(value)) then
-                value = (value) and 1 or 0
-            elseif (isTable(value)) then
-                value = "'" .. toJSON(value) .. "'"
-            elseif (isNil(value)) then
-                value = 'NULL'
-            end
-
-            values = values .. value
-            i = i + 1
+            keys[#keys+1] = key
+            value = toSQLValue(value)
+            values[#values+1] = value
         end
 
-        query = query .. ')' .. values .. ')'
-
+        local query = 'INSERT INTO `' .. self.tableName .. '` (`' .. table.concat(keys, '`, `') .. '`) VALUES (' .. table.concat(values, ', ') .. ')'
         local exec = prepareAndExecQuery(self.db:getConnection(), query)
         if (not exec) then
             error('DBManager: Error in query, can\'t create data', 2)
         end
 
-        self.datas[#self.datas+1] = data
+        self.datas[#self.datas + 1] = data
 
         outputDebugString('DB Manager RUNNING: ' .. query)
         return data
@@ -284,50 +260,49 @@ local crud = {
             error('DBManager: Invalid whereClauses (select)', 2)
         end
 
-        local results = {}
         local rows = self.datas
         local valuesInDB = self.valuesInDB
+        local results = {}
+        local resultsWithAttributes = {}
 
-        if (whereClauses and getTblSize(whereClauses) > 0) then
-            for _, row in ipairs(rows) do
-                local valid = true
-                for key, value in pairs(whereClauses) do
-                    if (not tblFind(valuesInDB, key)) then
-                        error('DBManager: Invalid attribute (select)', 2)
-                    end
-                    if (row[key] ~= value) then
-                        valid = false
-                        break
-                    end
-                end
-                if (valid) then
-                    results[#results+1] = row
-                    if (justOne) then
-                        break
-                    end
-                end
+        for _, attribute in ipairs(attributes or {}) do
+            if (not tblFind(valuesInDB, attribute)) then
+                error('DBManager: Invalid attribute (select)', 2)
             end
-        else
-            results = rows
         end
 
-        if (attributes and getTblSize(attributes) > 0) then
-            local attributeList = table.concat(attributes, ', ')
-            local resultsWithAttributes = {}
+        for _, row in pairs(rows) do
+            local valid = true
 
+            for key, value in pairs(whereClauses) do
+                if (not tblFind(valuesInDB, key)) then
+                    error('DBManager: Invalid attribute (select)', 2)
+                end
+                if (row[key] ~= value) then
+                    valid = false
+                    break
+                end
+            end
+
+            if (valid) then
+                results[#results+1] = row
+                if (justOne) then
+                    break
+                end
+            end
+        end
+
+        if (attributes and (#attributes > 0)) then
             for _, row in ipairs(results) do
                 local result = {}
                 for _, attribute in ipairs(attributes) do
-                    if (not tblFind(valuesInDB, attribute)) then
-                        error('DBManager: Invalid attribute (select)', 2)
-                    end
                     result[attribute] = row[attribute]
                 end
                 resultsWithAttributes[#resultsWithAttributes+1] = result
             end
-
             results = resultsWithAttributes
         end
+
         return results
     end,
 
@@ -347,25 +322,17 @@ local crud = {
 
         if (orderBy) then
             local orderType = (order == 'ASC') and 1 or -1
-
             table.sort(results, function(a, b)
-                if (orderType == 1) then
-                    return a[orderBy] < b[orderBy]
-                else
-                    return a[orderBy] > b[orderBy]
-                end
+                return orderType * (a[orderBy] < b[orderBy] and -1 or 1) < 0
             end)
         end
 
         if (limit) then
-            local resultsWithLimit = {}
-            for i, result in ipairs(results) do
-                if (i > offset + limit) then break end
-                if (i > offset) then
-                    resultsWithLimit[#resultsWithLimit + 1] = result
+            for i = #results, 1, -1 do
+                if (i <= offset or i > (offset + limit)) then
+                    table.remove(results, i)
                 end
             end
-            results = resultsWithLimit
         end
         return results
     end,
@@ -407,52 +374,51 @@ local crud = {
         end
         return results
     end,
-    
+
     update = function(self, data, options)
         if (not options or not isTable(options)) then
             error('DBManager: Invalid options (update)', 2)
         end
 
         local whereClauses = options.where or {}
-        local query = 'UPDATE `' .. self.tableName .. '` SET '
-        local i = 0
+        local queryParts = { 'UPDATE `', self.tableName, '` SET ' }
+        local values = {}
 
         for key, value in pairs(data) do
-            if (i > 0) then
-                query = query .. ', '
+            if (#values > 0) then
+                queryParts[#queryParts+1] = ', '
             end
 
-            if (isString(value)) then
-                value = "'" .. value .. "'"
-            elseif (isBoolean(value)) then
-                value = (value) and 1 or 0
-            elseif (isTable(value)) then
-                value = "'" .. toJSON(value) .. "'"
-            elseif (isNil(value)) then
-                value = 'NULL'
-            end
+            queryParts[#queryParts+1] = '`'
+            queryParts[#queryParts+1] = key
+            queryParts[#queryParts+1] = '` = ?'
 
-            query = query .. "`" .. key .. "` = " .. value
-            i = i + 1
+            values[#values+1] = toSQLValue(value)
         end
 
         if (self.db.data.dialect == 'sqlite') then
-            query = query .. ', `updated_at` = CURRENT_TIMESTAMP'
+            queryParts[#queryParts+1] = ', `updated_at` = CURRENT_TIMESTAMP'
         end
 
-        query = query .. ' WHERE '
+        queryParts[#queryParts+1] = ' WHERE '
 
-        i = 0
+        local i = 0
         for key, value in pairs(whereClauses) do
             if (i > 0) then
-                query = query .. ' AND '
+                queryParts[#queryParts+1] = ' AND '
             end
 
-            query = query .. "`" .. key .. "` = " .. value
+            queryParts[#queryParts+1] = '`'
+            queryParts[#queryParts+1] = key
+            queryParts[#queryParts+1] = '` = ?'
+
+            values[#values+1] = toSQLValue(value)
+
             i = i + 1
         end
 
-        local exec = prepareAndExecQuery(self.db:getConnection(), query)
+        local query = table.concat(queryParts)
+        local exec = dbExec(self.db:getConnection(), query, unpack(values))
         if (not exec) then
             error('DBManager: ERROR when updating data, please open the issue in GitHub', 2)
         end
@@ -491,34 +457,34 @@ local crud = {
 
         local whereClauses = options.where or {}
         local truncate = options.truncate or false
-        local query = 'DELETE FROM `' .. self.tableName .. '` WHERE '
-
-        local i = 0
-        for key, value in pairs(whereClauses) do
-            if (i > 0) then
-                query = query .. ' AND '
-            end
-
-            query = query .. "`" .. key .. "` = " .. value
-
-            i = i + 1
-        end
+        local query
 
         if (truncate) then
             query = 'TRUNCATE TABLE `' .. self.tableName .. '`'
+        else
+            query = 'DELETE FROM `' .. self.tableName .. '` WHERE '
+            local i = 0
+            for key, value in pairs(whereClauses) do
+                if (i > 1) then
+                    query = query .. ' AND '
+                end
+
+                query = query .. '`' .. key .. '` = ' .. toSQLValue(value)
+
+                i = i + 1
+            end
         end
 
         local exec = prepareAndExecQuery(self.db:getConnection(), query)
         if (not exec) then
-            error('DBManager: ERROR when deleting data, please open the issue in GitHub', 2)
+            error('DBManager: ERROR when destroying data, please open the issue in GitHub', 2)
         end
 
-        local rows = self.datas
         local valuesInDB = self.valuesInDB
-
-        for i, row in ipairs(rows) do
+        for i = #self.datas, 1, -1 do
+            local row = self.datas[i]
             local valid = true
-            for key, value in pairs(whereClauses) do
+            for key, value in ipairs(whereClauses) do
                 if (not tblFind(valuesInDB, key)) then
                     error('DBManager: Invalid attribute (destroy)', 2)
                 end
@@ -528,7 +494,7 @@ local crud = {
                 end
             end
             if (valid) then
-                table.remove(rows, i)
+                table.remove(self.datas, i)
             end
         end
 
@@ -610,16 +576,16 @@ function DBManager:new(data)
     if (not data) then
         error('DBManager: Data is required', 2)
     end
-    
+
     instance.data = data
-    
+
     if (instance.data.dialect == 'mysql') then
         instance.data.charset = instance.data.charset or 'utf8'
         instance.data.options = instance.data.options or ''
         instance.CONNECTION = dbConnect(
-            instance.data.dialect, 
-            'host=' .. instance.data.host .. ';port=' .. instance.data.port .. ';dbname=' .. instance.data.database .. ';charset=' .. instance.data.charset, 
-            instance.data.username, 
+            instance.data.dialect,
+            'host=' .. instance.data.host .. ';port=' .. instance.data.port .. ';dbname=' .. instance.data.database .. ';charset=' .. instance.data.charset,
+            instance.data.username,
             instance.data.password,
             instance.data.options
         )
@@ -660,7 +626,7 @@ function DBManager:query(queryString)
         return false
     end
 
-    return result, numAffectedRows, lastInsertId 
+    return result, numAffectedRows, lastInsertId
 end
 
 function DBManager:define(tableName, modelDefinition)
@@ -673,7 +639,7 @@ function DBManager:define(tableName, modelDefinition)
     instance.primaryKey = ""
     instance.valuesInDB = {}
     instance.datas = {}
-    
+
     local queryDefine = "CREATE TABLE IF NOT EXISTS `" .. instance.tableName .. "` ("
 
     local i = 0
@@ -681,7 +647,7 @@ function DBManager:define(tableName, modelDefinition)
         if (i > 0) then
             queryDefine = queryDefine .. ", "
         end
-        
+
         queryDefine = queryDefine .. "`" .. key .. "`"
 
         if (isTable(value)) then
@@ -705,11 +671,11 @@ function DBManager:define(tableName, modelDefinition)
         instance.valuesInDB[#instance.valuesInDB+1] = key
         i = i + 1
     end
-    
+
     if (instance.primaryKey == "") then
         error('DBManager: No primary key defined', 2)
     end
-    
+
     if (instance.db.dialect == 'mysql') then
         queryDefine = queryDefine .. ", `created_at` DATETIME DEFAULT CURRENT_TIMESTAMP"
         queryDefine = queryDefine .. ", `updated_at` DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP"
@@ -717,7 +683,7 @@ function DBManager:define(tableName, modelDefinition)
         queryDefine = queryDefine .. ", `created_at` DATETIME DEFAULT CURRENT_TIMESTAMP"
         queryDefine = queryDefine .. ", `updated_at` DATETIME DEFAULT CURRENT_TIMESTAMP"
     end
-    
+
     instance.valuesInDB[#instance.valuesInDB+1] = 'created_at'
     instance.valuesInDB[#instance.valuesInDB+1] = 'updated_at'
 
